308a7c7814ff744b4d2be36ae2119c60f727c97e
Pierre Fernique <pfernique@gmail.com>
2017-11-17 11:39:36 +0100

    Update build.sh

--- bin/conda/python-softmaint/build.sh
+++ bin/conda/python-softmaint/build.sh
@@ -24,7 +24,7 @@ set -ev
 
 if [[ "$PY3K" = 1 ]]; then
   2to3 -n -w $SRC_DIR/src/py/softmaint
-  2to3 -n -w $SRC_DIR/test
+  # 2to3 -n -w $SRC_DIR/test
 fi
 
 $PYTHON setup.py install --prefix=$PREFIX

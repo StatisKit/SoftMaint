b18d3b09b127c0b5ed0379d596e558af4ef5b49a
Pierre Fernique <pfernique@gmail.com>
2017-11-17 11:39:47 +0100

    Update bld.bat

--- bin/conda/python-softmaint/bld.bat
+++ bin/conda/python-softmaint/bld.bat
@@ -24,7 +24,7 @@ echo ON
 
 if "%PY3K%" == "1" (
   2to3 -n -w %SRC_DIR%\src\py\softmaint
-  2to3 -n -w %SRC_DIR%\test
+  :: 2to3 -n -w %SRC_DIR%\test
 )
 
 %PYTHON% setup.py install --prefix=%PREFIX%
